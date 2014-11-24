3b38b2fd89808f4791b1d777aa92558a4d3eb9f5
Scott Corgan <scottcorgan@gmail.com>
2014-11-24 08:28:01 -0800

    Always install latest 1.x for Superstatic and Divshot-cli. Closes #13. Closes #14.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,16 +47,4 @@ module.exports = function(grunt) {
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  // grunt.registerTask('test', ['clean', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  // grunt.registerTask('default', ['jshint', 'test']);
 };
